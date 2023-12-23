55148cff3ba48d4ea2bc064cea12fb87374ae256
Marta109 <marta.hayrapetyan.a@gmail.com>
2023-12-23 06:06:30 +0400

    feat: add answers

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const {ListNode} = require("../extensions/list-node.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+// class ListNode {
+//   constructor(x) {
+//     this.value = x;
+//     this.next = null;
+//   }
+// }
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.next = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    if (!this.next) return;
+    return this.head;
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value);
+
+    if (!this.head) {
+      this.head = node;
+    }
+
+    if (this.next) {
+      this.next.next = node;
+    }
+
+    this.next = node;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) return;
+    const head = this.head.value;
+    this.head = this.head.next;
+    return head;
   }
 }
 
+// const queue = new Queue();
+// queue.enqueue(1);
+// queue.enqueue(3);
+// queue.enqueue(5);
+// queue.enqueue(6);
+// queue.dequeue();
+// console.log(queue);
+// console.log(queue.dequeue());
+// console.log(queue);
+// console.log(queue.getUnderlyingList());
+
 module.exports = {
-  Queue
+  Queue,
 };
